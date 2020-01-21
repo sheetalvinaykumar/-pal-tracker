3b885c218a8de2b90a4830911d852de8082d77d4
Sheetal Vinay Kumar <sheetalvinaykumar@gmail.com>
2020-01-21 10:40:17 +0530

    environment variables

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -13,6 +13,22 @@ using Microsoft.Extensions.Options;
 
 namespace PalTracker
 {
+    public static class CloudFoundryInfoExtensions
+    {
+        public static IServiceCollection CloudFoundryInfoConfigureServices(this IServiceCollection services, IConfiguration Configuration)
+        {
+            string port = Configuration.GetValue<string>("PORT", "PORT not configured.");
+            string memoryLimit = Configuration.GetValue<string>("MEMORY_LIMIT", "MEMORY_LIMIT not configured.");
+            string cfInstanceIndex = Configuration.GetValue<string>("CF_INSTANCE_INDEX", "CF_INSTANCE_INDEX not configured.");
+            string cfInstanceAddr = Configuration.GetValue<string>("CF_INSTANCE_ADDR", "CF_INSTANCE_ADDR not configured.");
+        
+        
+            CloudFoundryInfo info = new CloudFoundryInfo(port, memoryLimit,cfInstanceIndex, cfInstanceAddr);
+            services.AddSingleton(info);
+
+            return services;
+        }
+    }
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -26,8 +42,16 @@ namespace PalTracker
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
 
+            string message = Configuration.GetValue<string>("WELCOME_MESSAGE", "WELCOME_MESSAGE not configured.");
+
+            services
+            .AddSingleton(sp => new WelcomeMessage(message))
+            .CloudFoundryInfoConfigureServices(Configuration);
+
+             
+        }
+       
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
